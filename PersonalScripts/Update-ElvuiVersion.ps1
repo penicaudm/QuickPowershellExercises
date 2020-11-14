3d089b397be1c8d1e69997c52bbc1308ce83925e
penicaudm <marcellin.penicaud@gmail.com>
2020-11-14 14:37:43 +0100

    clean up script & setup first run

--- PersonalScripts/Update-ElvuiVersion.ps1
+++ PersonalScripts/Update-ElvuiVersion.ps1
@@ -4,14 +4,17 @@ param(
     [uri]$ElvuiAPIUri = "https://www.tukui.org/api.php?ui=elvui",
 
     [Parameter ()]
-    [validateScript (
-        {
-            Test-path $_
-        }
-    )]
     [string]$FileVersionFile = "C:\Scripts\AddonManagement\ElvuiVersion.txt"
 )
+#requires -version 5.1
 #region Functions
+Function Intialize-ElvUIUpdater
+{
+    param(
+        [string]$LocalVersionFile = "C:\Scripts\AddonManagement\ElvuiVersion.txt"
+    )
+    New-Item -Path $LocalVersionFile -Force
+}
 function Get-WoWRootFOlder
 {
     $InstallPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Blizzard Entertainment\World of Warcraft" | Select-Object -ExpandProperty InstallPath
@@ -58,23 +61,46 @@ function Update-ElvUI
                 Test-path $_
             }
         )]
-        [string] $InterfaceFolder
+        [string] $InterfaceFolder,
+
+        [string] $TempFile = "$env:TEMP\Elvui.zip"
     )
+    if (Test-Path $TempFile)
+    {
+        Remove-Item $TempFile
+    }
     Invoke-RestMethod -Uri $ArchiveURI -OutFile $env:TEMP\Elvui.zip -Method Get -ErrorAction Stop
 
-    Expand-Archive -Path $env:TEMP\Elvui.zip -DestinationPath $InterfaceFOlder -Force
+    Expand-Archive -Path $env:TEMP\Elvui.zip -DestinationPath $InterfaceFOlder -Force -
+
+    Remove-Item 
 }
 #endregion
 #region Static
 $ElvuiData = Invoke-RestMethod -Uri $ElvuiAPIUri
 $CurrentVersion = [float]$ElvuiData.Version
-
 $InstalledVersion = [float](Get-Content -Path $FileVersionFile)
+#endregion
+# Run the script
+if (-not (Test-Path $FileVersionFile) )
+{
+    Write-Verbose "Running for the first time, setting up files"
+    Intialize-ElvUIUpdater
+
+    Update-ElvUI -ArchiveURI $ElvuiData.url -InterfaceFolder (Get-WoWRootFOlder)
+
+    Send-WindowsToast -Version $CurrentVersion
+
+    Set-Content -Force -Path $FileVersionFile -Value $CurrentVersion
+}
+Write-Verbose "Available version is $currentversion"
+Write-Verbose "Installed Version is $InstalledVersion"
 # If the available version is higher than installed, update the addon and the file
 if ($CurrentVersion -gt $InstalledVersion)
 {
     Update-ElvUI -ArchiveURI $ElvuiData.url -InterfaceFolder (Get-WoWRootFOlder)
 
     Send-WindowsToast -Version $CurrentVersion
+    
     Set-Content -Force -Path $FileVersionFile -Value $CurrentVersion
 }
